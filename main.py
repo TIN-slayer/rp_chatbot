cc167531aa806fd4b1a6e86c40eda00e21db6f78
TIN-slayer <dimka_dmit@mail.ru>
2021-06-04 18:34:07 +0300

    mvp

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ in_game_ids = []
 mass_id_inroom = {1: [],
                   2: []}
 locations = ['город', 'горы']
+qtty = 0
 
 
 @bot.message_handler(commands=['start'])
@@ -35,11 +36,12 @@ def help_message(message):
     if in_game_ids.count(message.from_user.id) < 1:
         bot.send_message(message.chat.id,
                          'Чтобы войти в игру напиши /new_character, после чего ты создашь персонажа для евента, а затем войдешь в игру.\n'
-                         'Чтобы узнать, что это за бот, напиши /start\n'
-                         'Чтобы узнать подробнее про идущий евент, напиши /event_info')
+                         'Чтобы узнать, что это за бот, напиши /start')
+        # 'Чтобы узнать подробнее про идущий евент, напиши /event_info'
     else:
-        bot.send_message(message.chat.id, 'Чтобы узнать, что это за бот, напиши /start\n'
-                                          'Чтобы узнать подробнее про идущий евент, напиши /event_info')
+        bot.send_message(message.chat.id, 'Чтобы узнать, что это за бот, напиши /start')
+        # 'Чтобы узнать подробнее про идущий евент, напиши /event_info\n'
+        # 'Чтобы перейти в другую лоуацию напишите /location'
 
 
 @bot.message_handler(commands=['new_character'])
@@ -60,28 +62,39 @@ def create_character(message):
         con.commit()
 
 
-@bot.message_handler(commands=['location'])
-def location_select(message):
-    global commands_state
-    bot.send_message(message.from_user.id, 'Выберите локацию, к которой хотите присоединиться:\n'
-                                           '1.Город\n'
-                                           '2.Горы')
-    commands_state = 'move_to_another_location'
+# @bot.message_handler(commands=['check_info'])
+# def check(message):
+#     con = sql.connect('data.db')
+#     cur = con.cursor()
+#     bot.send_message(message.from_user.id, 'Выберите чела для проверки его инфы')
+#     cur.execute(f'update users set state = \'check_info\' where id = {message.from_user.id}')
+#     con.commit()
+#
+# @bot.message_handler(commands=['location'])
+# def location_select(message):
+#     bot.send_message(message.from_user.id, 'Выберите локацию, к которой хотите присоединиться:\n'
+#                                            '1.Город\n'
+#                                            '2.Горы')
+#     # 'move_to_another_location'
 
 
 @bot.message_handler(content_types=['text'])
 def default_text(message):
+    global qtty
     con = sql.connect('data.db')
     cur = con.cursor()
     cur.execute(f'select state from users where id = {message.from_user.id}')
     res = cur.fetchall()
     state = res[0][0]
     if state == 'default':
+        qtty += 1
         cur.execute(f'select id from users')
         sent = cur.fetchall()
+        cur.execute(f'select name, race, class from users where id = {message.from_user.id}')
+        player = cur.fetchall()[0]
         for i in sent:
             if int(i[0]) != message.from_user.id:
-                bot.send_message(int(i[0]), message.text)
+                bot.send_message(int(i[0]), f'{player[0]}, {player[1]}, {player[2]}\n' + message.text)
     elif state == 'getting_info':
         sp = message.text.split('|')
         print(f'update users set state = \'{sp}\' where id = {message.from_user.id}')
@@ -90,13 +103,29 @@ def default_text(message):
         con.commit()
         cur.execute(f'update users set state = \'default\' where id = {message.from_user.id}')
         con.commit()
-    elif state == 'move_to_another_location':
-        if message.text in [1, 2]:
-            mass_id_inroom[message.text].append(message.from_user.id)
-            bot.send_message(message.from_user.id,
-                             f'Вы теперь находитесь в локации {locations[int(message.text) - 1]}.')
-        else:
-            bot.send_message(message.from_user.id, 'Это не так локация которую вы ищете (Такой локации нет)')
+    # elif state == 'check_info':
+    #     pass
+    # elif state == 'move_to_another_location':
+    #     if message.text in [1, 2]:
+    #         mass_id_inroom[message.text].append(message.from_user.id)
+    #         bot.send_message(message.from_user.id,
+    #                          f'Вы теперь находитесь в локации {locations[int(message.text) - 1]}.')
+    #     else:
+    #         bot.send_message(message.from_user.id, 'Это не так локация которую вы ищете (Такой локации нет)')
+    if qtty == 5:
+        cur.execute(f'select arc_2 from events where id = {ev}')
+        arc_2 = cur.fetchall()[0][0]
+        cur.execute(f'select id from users')
+        sent = cur.fetchall()
+        for i in sent:
+            bot.send_message(int(i[0]), f'{arc_2}')
+    elif qtty == 10:
+        cur.execute(f'select arc_3 from events where id = {ev}')
+        arc_3 = cur.fetchall()[0][0]
+        cur.execute(f'select id from users')
+        sent = cur.fetchall()
+        for i in sent:
+            bot.send_message(int(i[0]), f'{arc_3}')
 
 
 bot.polling(none_stop=True)
