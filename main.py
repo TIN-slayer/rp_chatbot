472ed1eebf06027188b5a4f24d5c5775c5c53a96
Totrg <belogolov2005@mail.ru>
2021-06-04 14:05:35 +0300

    added_command_help

--- main.py
+++ main.py
@@ -4,9 +4,26 @@ import sqlite3 as sql
 
 bot = telebot.TeleBot('1833045242:AAEZcG7f1HDnz-sU_AFYbPNDeRNO5GcrW1Q')
 
+event = 'Надвигающаяся гроза'
 
-@bot.message_handler(commands=['start', 'help'])
+in_game_ids = []
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'hello and about')
+    bot.send_message(message.chat.id, f'Привет! Я бот для рп-игр. Сейчас идёт ивент Name. Сейчас происходит: "{event}". Напиши /new_character чтобы создать анкету.')
 
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    if in_game_ids.count(message.from_user.id) < 1:
+        bot.send_message(message.chat.id, 'Чтобы войти в игру напиши /new_character, после чего ты создашь персонажа для евента, а затем войдешь в игру.\n'
+                                          'Чтобы узнать, что это за бот, напиши /start\n'
+                                          'Чтобы узнать подробнее про идущий евент, напиши /event_info')
+    else:
+        bot.send_message(message.chat.id, 'Чтобы узнать, что это за бот, напиши /start\n'
+                         'Чтобы узнать подробнее про идущий евент, напиши /event_info')
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, f'Привет! Я бот для рп-игр. Сейчас идёт ивент Name. Сейчас происходит: "{event}". Напиши /new_character чтобы создать анкету.')
+    
 bot.polling(none_stop=True)
